8a0ad820762b0644011ffb22a75979c90d58fbc8
nhatanh222k <nhatanh222006@gmail.com>
2026-08-06 19:56:30 +0700

    b3
    b3

---
+++ b3/b3/b3.cpp
@@ -0,0 +1,22 @@
+// b3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<conio.h>
+
+int main()
+{
+    std::cout << "A\t B\t C\t D\t E\t F\t\nG\t H\t I\t J\t K\t L\t\nM\t N\t O\t P\t Q\t R\t\nS\t T\t U\t V\t W\t X\t\nY\t Z\t";
+    _getch();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
