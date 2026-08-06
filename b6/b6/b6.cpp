674e42d9d2d20c4b0c377280c11aebb2f1ee9496
nhatanh222k <nhatanh222006@gmail.com>
2026-08-06 20:29:45 +0700

    b6
    b6

---
+++ b6/b6/b6.cpp
@@ -0,0 +1,35 @@
+// b6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+    std::cout << "======================================================\n";
+    std::cout << "*                    THUC DON                        *\n";
+    std::cout << "======================================================\n";
+    std::cout << "*             1. Coca cola....12.000                 *\n";
+    std::cout << "*             2. Nuoc suoi.....7.000                 *\n";
+    std::cout << "*             3. Cam ep.......15.000                 *\n";
+    std::cout << "*             4. Sua tuoi.....12.000                 *\n";
+    std::cout << "*             5. Pepsi........12.000                 *\n";
+    std::cout << "*             6. C2...........12.000                 *\n";
+    std::cout << "*             7. Sting dau....12.000                 *\n";
+    std::cout << "*             8. Tra sua dao..50.000                 *\n";
+    std::cout << "*             9. Tra chanh....15.000                 *\n";
+    std::cout << "*             10.sinh to dau..25.000                 *\n";
+    std::cout << "======================================================\n";
+    _getch();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
