1de41fd5d7c9e9e6881ede1bb01219d4a5b06ce1
nhatanh222k <nhatanh222006@gmail.com>
2026-08-06 19:49:06 +0700

    b2
    b2

---
+++ b2/b2/b2.cpp
@@ -0,0 +1,22 @@
+// b2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<conio.h>
+
+int main()
+{
+    std::cout << "1. In C, lowercase letters are significant.\n2. main is where program execution begins.\n3. Opening and closing braces enclose program statements in a routine.\n4.All program statements must be terminated by a semicolon.";
+    _getch();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
