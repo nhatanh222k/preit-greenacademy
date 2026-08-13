257a9f6df4b67bc77c1e2ff01b3264e8aa71d56c
TenCuaBan <emailcuaban@gmail.com>
2026-08-13 17:31:59 +0700

    a
    a

---
+++ 11.8/11.8(2.5)/11.8(2.5).cpp
@@ -0,0 +1,37 @@
+// 11.8(2.5).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    double c;
+    cout << "Nhap a:";
+    cin >> a;
+    cout << "Nhap b:";
+    cin >> b;
+    if (a == 0 and b == 0) {
+        cout << "phuong trinh vo so nghiem";
+    }
+    else if (a == 0 and b != 0) {
+        cout << "Phuong trinh vo nghiem";
+    }
+    else {
+        c = (-b) / a;
+        cout << "phuong trinh co nghiem la " << c;
+    }
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
